64f28567e4521f1524cd4903bab58e7e3a7e1d40
Jack5964 <Jack5964@7f6f2bf4-a0bc-fe98-129f-e413ec76cb38>
2010-06-08 09:33:46 +0000

    first import

---
+++ trunk/src/ibsta/LiveZone/ZoneItem.java
@@ -0,0 +1,14 @@
+package ibsta.LiveZone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ZoneItem extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.zoneitem);
+	}
+		
+}
